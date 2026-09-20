30f5ad5c330682bb591a2ba74b3f69eddfaefe53
Mert <manausxv@gmail.com>
2026-09-20 09:53:43 +0300

    Add layout template with styled navbar and connect Overview page

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from dashboard.models import (
+    FinancialYear, SegmentPerformance, ProductRevenue, GeographyRevenue,
+    StoreCountYear, StockQuarter, LoyaltyQuarter, MenuDrink, MenuFood, StoreLocation
+)
+admin.site.register(FinancialYear)
+admin.site.register(SegmentPerformance)
+admin.site.register(ProductRevenue)
+admin.site.register(GeographyRevenue)
+admin.site.register(StoreCountYear)
+admin.site.register(StockQuarter)
+admin.site.register(LoyaltyQuarter)
+admin.site.register(MenuDrink)
+admin.site.register(MenuFood)
+admin.site.register(StoreLocation)
